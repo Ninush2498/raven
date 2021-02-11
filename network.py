68f170b2443bef1e17b3bc19612f5e8ecd98fe0c
Nina Raganová <57150586+Ninush2498@users.noreply.github.com>
2021-02-11 01:24:53 +0100

    Update network.py

--- network.py
+++ network.py
@@ -1,30 +1,13 @@
-#zakomentovane poznamky by mali byt implementovane
-#nezakomentovane este treba pridat
-
-#Poznamky:
-#fully connected layer randomly initialized
-#extra dropout layer before fully connected - default value 0.5
-#mini-batch = 32
-#Adam optimizer
-#rate 0.0002, reducing to half every 10 iterations
-#images 224 x 224, normalized to range <0,1>
-#6 training sets, 2 validation, 2 testing
-#freeze all batch normalization layers during training
-#binary cross entropy
-#sigmoid for normalization
-
-#Kod:
 import torchvision.models as models
 import torch.nn as nn
 import torch.optim as optim
 from util import *
 
-def get_nn(device, pretrained=False):
+def get_nn(pretrained=False):
     model = models.resnet18()
-    model_trained = models.resnet18(pretrained=True) #tento pouzili
 
     if pretrained:
-        model = model_trained
+        model = models.resnet18(pretrained=True) #tento pouzili
         
     model.fc = nn.Sequential(
         nn.Dropout(0.5),
@@ -32,17 +15,10 @@ def get_nn(device, pretrained=False):
 
     freeze_BatchNorm2d(model)
 
-    batch_size = 32
-    feature_extract = False
     loss = nn.BCELoss(reduction='sum')
 
     optimizer = optim.Adam(model.parameters(), lr=0.0002)
-    lambda_function = lambda epoch : 1 if epoch%10==0 else 0.5
-    scheduler = optim.lr_scheduler.MultiplicativeLR(optimizer, lambda_function)
+    change_rate = lambda epoch : 0.5 if epoch%10==0 else 1
+    scheduler = optim.lr_scheduler.MultiplicativeLR(optimizer, change_rate)
 
     return model, loss, optimizer, scheduler
-
-
-
-
-
