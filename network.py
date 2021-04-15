1a1e1ff6644c13837f61423695a8025cb3359375
Nina Raganová <57150586+Ninush2498@users.noreply.github.com>
2021-04-15 16:33:58 +0200

    Update network.py

--- network.py
+++ network.py
@@ -3,22 +3,21 @@ import torch.nn as nn
 import torch.optim as optim
 from util import *
 
-def get_nn(pretrained=False):
-    model = models.resnet18()
-
-    if pretrained:
-        model = models.resnet18(pretrained=True) #tento pouzili
+def get_nn(device=torch.device('cuda'), pretrained=False):
+    model = models.resnet18(pretrained)
         
     model.fc = nn.Sequential(
         nn.Dropout(0.5),
-        nn.Linear(512, 1))
-
+        nn.Linear(512,1))
+    
     freeze_BatchNorm2d(model)
+    model.to(device)
 
-    loss = nn.BCELoss(reduction='sum')
+    loss = nn.BCELoss('sum')
 
     optimizer = optim.Adam(model.parameters(), lr=0.0002)
     change_rate = lambda epoch : 0.5 if epoch%10==0 else 1
     scheduler = optim.lr_scheduler.MultiplicativeLR(optimizer, change_rate)
 
     return model, loss, optimizer, scheduler
+
